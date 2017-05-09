8d7c1913fd6951819bd790fef6174f4572ba6e23
Leothon Wong <langmancaizi@gmail.com>
2017-05-09 21:16:13 +0800

    增加搜索栏界面

---
+++ app/src/main/java/demo/com/zhihu/UI/Activity/searcherActivity.java
@@ -0,0 +1,13 @@
+package demo.com.zhihu.UI.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class searcherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searcher);
+    }
+}
